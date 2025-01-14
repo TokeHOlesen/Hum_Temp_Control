6303403a80dacdb1fad3a32c4a0d16dce3da9286
Toke H. Olesen <tokeholesen@gmail.com>
2025-01-14 15:42:09 +0100

    Temperature and humidity control refinements

--- relays_class.py
+++ relays_class.py
@@ -27,21 +27,32 @@ class Relays:
             self.ventilator_ch1.on()
             
             if self.ventilator_ch1.is_lit:
-                # Only turns the heater on if the temperature is lower than target, minus acceptable tolerance
+                # If the measured temperature is below target (minus the value defined in TEMPERATURE_TOLERANCE), turns
+                # the heater on. Turns it off once it reaches target + TEMPERATURE_TOLERANCE.
+                # For example, if target temperature is 40 and tolerance is 2, the heater will turn on if the
+                # temperature is below or at 38 degrees, and turn off again once it reaches 42 degrees.
+                # It will then turn on again only when the temperature falls below 38 degrees.
                 if sensor_data.current_temp_dht <= user_input.target_temp - constants.TEMPERATURE_TOLERANCE:
                     self.varmer_ch2.on()
-                else:
+                elif sensor_data.current_temp_dht >= user_input.target_temp + constants.TEMPERATURE_TOLERANCE:
                     self.varmer_ch2.off()
                 
-                # Checks if the current temperature is within a set range of temperatures from target
-                # Humidifier and dehumidifier won't be turned on otherwise
+                # Runs humidity control if the temperature is in a range defined in HUMIDITY_CONTROL_THRESHOLD.
+                # For example, it target temperature is 40 and HUMIDITY_CONTROL_THRESHOLD is set to 10, humidity
+                # control will only run when the temperature is in the range 30-50 degrees.
                 if abs(sensor_data.current_temp_dht - user_input.target_temp) <= constants.HUMIDITY_CONTROL_THRESHOLD:
+                    # Turns humidifier on if humidity is below (target - tolerance)
                     if sensor_data.current_hum_dht < user_input.target_humidity - constants.HUMIDITY_TOLERANCE:
-                        self.affugter_ch3.off()
                         self.damp_ch4.on()
-                    elif sensor_data.current_hum_dht > user_input.target_humidity + constants.HUMIDITY_TOLERANCE:
-                        self.affugter_ch3.on()
+                    # Turns humidifier off is humidity is at or above target
+                    if sensor_data.current_hum_dht >= user_input.target_humidity:
                         self.damp_ch4.off()
+                    # Turns dehumidifier on if humidity is above (target + tolerance)
+                    if sensor_data.current_hum_dht > user_input.target_humidity + constants.HUMIDITY_TOLERANCE:
+                        self.affugter_ch3.on()
+                    # Turns dehumidifier off is humidity is at or below target
+                    if sensor_data.current_hum_dht <= user_input.target_humidity:
+                        self.affugter_ch3.off()
                 else:
                     self.affugter_ch3.off()
                     self.damp_ch4.off()

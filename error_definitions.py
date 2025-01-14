e1a476c7a0c0d6afbb43aac2eeddd0ad9b5b36ca
Toke H. Olesen <tokeholesen@gmail.com>
2025-01-14 14:41:14 +0100

    Error definitions added

---
+++ error_definitions.py
@@ -0,0 +1,8 @@
+class ValueOutsideRangeError(Exception):
+    def __init__(self, message="Value is outside the allowed range."):
+        super().__init__(message)
+
+
+class ValueMissingError(Exception):
+    def __init__(self, message="No value was provided."):
+        super().__init__(message)
